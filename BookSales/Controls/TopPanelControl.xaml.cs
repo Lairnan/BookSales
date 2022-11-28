e8cb293928a17d42988eb8ede797567e9fa458ba
Alexey Saulin <alexey.saulin03@mail.ru>
2022-11-28 20:04:11 +0300

    Add TopPanelControl for window moves

---
+++ BookSales/Controls/TopPanelControl.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interop;
+
+namespace BookSales.Controls
+{
+
+	public partial class TopPanelControl : UserControl
+	{
+		public TopPanelControl()
+		{
+			InitializeComponent();
+		}
+
+		[DllImport("user32.dll")]
+		private static extern IntPtr SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+		private void PnlControlBar_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			if (e.ClickCount == 2)
+			{
+				SwitchState();
+				return;
+			}
+
+			var window = (this.Parent as Grid).Parent as Window;
+			var helper = new WindowInteropHelper(window);
+			SendMessage(helper.Handle, 161, 2, 0);
+			WindowCurrentState.Text = window.WindowState == WindowState.Maximized ? "2" : "1";
+		}
+
+		private void PnlControlBar_OnMouseEnter(object sender, MouseEventArgs e)
+		{
+			var window = (this.Parent as Grid).Parent as Window;
+			window.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+		}
+
+		private void CloseBtn_Click(object sender, RoutedEventArgs e)
+		{
+			Application.Current.Shutdown();
+		}
+
+		private void MinimizeBtn_Click(object sender, RoutedEventArgs e)
+		{
+			var window = (this.Parent as Grid).Parent as Window;
+			window.WindowState = WindowState.Minimized;
+		}
+
+		private void SwitchStateBtn_Click(object sender, RoutedEventArgs e)
+		{
+			SwitchState();
+		}
+
+		private void SwitchState()
+		{
+			var window = (this.Parent as Grid).Parent as Window;
+			window.WindowState =
+				window.WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+			WindowCurrentState.Text = window.WindowState == WindowState.Maximized ? "2" : "1";
+		}
+	}
+}
